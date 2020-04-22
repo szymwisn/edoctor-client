b62b4b05923509afb3427a4344510f31fbae49ad
szymwisn@gmail.com <szymwisn@gmail.com>
2020-04-22 18:49:28 +0200

    Basic project setup

---
+++ src/app/_shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputComponent } from './components/form/input/input.component';
+import { SelectComponent } from './components/form/select/select.component';
+import { TextareaComponent } from './components/form/textarea/textarea.component';
+import { CheckboxComponent } from './components/form/checkbox/checkbox.component';
+import { NotificationComponent } from './components/notification/notification.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ButtonComponent } from './components/form/button/button.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { IconComponent } from './components/sidebar/icon/icon.component';
+import { SearchComponent } from './components/search/search.component';
+
+const items = [
+  InputComponent,
+  SelectComponent,
+  TextareaComponent,
+  CheckboxComponent,
+  NotificationComponent,
+  ModalComponent,
+  ButtonComponent,
+  PaginationComponent,
+  SidebarComponent,
+  BreadcrumbComponent,
+  LogoComponent,
+  IconComponent,
+  SearchComponent,
+];
+
+@NgModule({
+  declarations: [...items],
+  imports: [CommonModule],
+  exports: [...items],
+})
+export class SharedModule {}
