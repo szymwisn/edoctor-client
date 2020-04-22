82f77ff7e892404e7e5d6e7f748c90027a3b43a9
szymwisn@gmail.com <szymwisn@gmail.com>
2020-04-22 20:44:26 +0200

    PROJ-39 Added user model and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -8,7 +9,13 @@ import { PagesModule } from "./pages/pages.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [SharedModule, BrowserModule, AppRoutingModule, PagesModule],
+  imports: [
+    SharedModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PagesModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
