dc26f28f541ffdbd485f4e78db7c0f53d8637f14
szymwisn@gmail.com <szymwisn@gmail.com>
2020-04-30 19:53:00 +0200

    PROJ-27 Preparation to working on landing page

--- src/app/_shared/components/sidebar/sidebar.component.ts
+++ src/app/_shared/components/sidebar/sidebar.component.ts
@@ -1,8 +1,15 @@
 import { Component } from "@angular/core";
+import { UserFacade } from "../../security/user.facade";
 
 @Component({
   selector: "app-sidebar",
   templateUrl: "./sidebar.component.html",
   styleUrls: ["./sidebar.component.scss"],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private userFacade: UserFacade) {}
+
+  logout() {
+    this.userFacade.signout();
+  }
+}
