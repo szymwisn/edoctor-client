7c38d22746700fc19b79abbe46fc8d345dda53e5
szymwisn@gmail.com <szymwisn@gmail.com>
2020-05-02 13:27:54 +0200

    PROJ-34 Added Register Page

--- src/app/pages/register-page/containers/register-page/register-page.component.ts
+++ src/app/pages/register-page/containers/register-page/register-page.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { UserFacade } from "src/app/_shared/security/user.facade";
+import { Sex } from "src/app/_shared/security/models/sex";
+import { BloodType } from "src/app/_shared/security/models/blood-type";
 
 @Component({
-  selector: 'app-register-page',
-  templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+  selector: "app-register-page",
+  templateUrl: "./register-page.component.html",
+  styleUrls: ["./register-page.component.scss"],
 })
-export class RegisterPageComponent implements OnInit {
+export class RegisterPageComponent {
+  form: FormGroup;
+  formSubmitted = false;
 
-  constructor() { }
+  sexes: Sex[] = [Sex.Female, Sex.Male, Sex.Other];
+  bloodTypes: BloodType[] = [
+    BloodType.A_Plus,
+    BloodType.A_Minus,
+    BloodType.B_Plus,
+    BloodType.B_Minus,
+    BloodType.AB_Plus,
+    BloodType.AB_Minus,
+    BloodType.Zero_Plus,
+    BloodType.Zero_Minus,
+  ];
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private userFacade: UserFacade) {
+    this.form = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", Validators.required],
+      age: ["", [Validators.required]],
+      height: ["", [Validators.required]],
+      mass: ["", [Validators.required]],
+      bloodType: ["", [Validators.required]],
+      sex: ["", [Validators.required]],
+    });
   }
 
+  register() {
+    this.formSubmitted = true;
+    if (this.form.valid) {
+      this.userFacade.register(this.form.value);
+      this.formSubmitted = false;
+    }
+  }
 }
