fc8541768de992169f7f2e693708719920e6cdc5
szymwisn@gmail.com <szymwisn@gmail.com>
2020-05-02 16:52:35 +0200

    PROJ-86 Added Error Page

---
+++ src/app/_shared/components/toolbox/toolbox.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { Toolbox } from "./model/toolbox.model";
+
+@Component({
+  selector: "app-toolbox",
+  templateUrl: "./toolbox.component.html",
+  styleUrls: ["./toolbox.component.scss"],
+})
+export class ToolboxComponent {
+  boxes: Toolbox[] = [
+    {
+      title: "Healthcheck",
+      link: "healthcheck",
+    },
+    {
+      title: "Change your data",
+      link: "profile",
+    },
+    {
+      title: "Doctors nearby",
+      link: "doctors",
+    },
+    {
+      title: "View history",
+      link: "history",
+    },
+  ];
+}
--- src/app/pages/error-page/error-page.module.ts
+++ src/app/pages/error-page/error-page.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ErrorPageComponent } from './containers/error-page/error-page.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ErrorPageComponent } from "./containers/error-page/error-page.component";
+import { SharedModule } from "src/app/_shared/shared.module";
 
 @NgModule({
   declarations: [ErrorPageComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, SharedModule],
 })
-export class ErrorPageModule { }
+export class ErrorPageModule {}
